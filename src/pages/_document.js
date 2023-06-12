0aabdda75ff51fcc5a24006fcf7c30a654c57b4e
souvik150 <souvikmukherjee150@gmail.com>
2023-06-13 01:51:09 +0530

    fix: ui

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <title>ic-ETITE '24</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
