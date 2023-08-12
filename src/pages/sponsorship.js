42f4616df31bd11c2b2cef89d327af5978c4f220
souvik150 <souvikmukherjee150@gmail.com>
2023-08-13 02:59:32 +0530

    fix: update sponsorships

--- src/pages/sponsorship.js
+++ src/pages/sponsorship.js
@@ -47,82 +47,107 @@ const Sponsorship = () => {
         </h1>
         <div className="flex flex-col md:flex-row lg:flex-row items-start justify-between text-2xl">
           <p>Platinum Sponsor</p>
-          <p>₹ 1,00,000</p>
+          <p>₹ 5,00,000</p>
         </div>
         <div className="text-lg py-10">
-          <p>&rarr; All benefits of Gold Sponsor.</p>
-          <p>&rarr; One special demonstration display area.</p>
+          <p>&rarr; Company will be identified as the Title Sponsor for ic-ETITE 2024.</p>
+          <p>&rarr; Branding of the Company around the campus during the fest through LED screens (more than 50-60 times a day).</p>
           <p>&rarr; One full page advertisement in the conference program.</p>
-          <p>&rarr; Permanent recognition in conference proceedings.</p>
+          <p>&rarr; Publicity on campus at the Academic Blocks (during Pre-ic-ETITE and all two days of ic-ETITE).</p>
           <p>
-            &rarr; Prominent recognition in the conference programs (front/back
-            covers).
+            &rarr; Nation-wide circulation of commercials through our online media partners.
           </p>
           <p>
-            &rarr; Ability to distribute Information in attendee registration
-            packets.
+            &rarr; Marketing through standees inside the campus everywhere.
           </p>
           <p>
             &rarr; Speaker time pre/post conference with non-competing program.
           </p>
           <p>
-            &rarr; Facility space that may be setup for tabs/hands on workshops
-            pre/post program.
+            &rarr; Advertisement on the ic-ETITE’24 website and mobile app.
           </p>
           <p>
-            &rarr; Ability to submit articles of interest in the pre-conference
-            e-mail to attendees.
+            &rarr; Name and Logo on the T-Shirt of the conference.
           </p>
           <p>
             &rarr; Sponsor webpage on conference website, content to be
             delivered by sponsor to specific conference webmaster once approved
             by conference committee.
           </p>
+          <p>
+            &rarr; Judge for the Technext Exhibition and Bolt hackathon.
+          </p>
+          <p>
+            &rarr; Speaker session and Tailored event during the conference.
+          </p>
+          <p>
+            &rarr; Stalls 10x10 sq ft. for two days.
+          </p>
         </div>
 
         <div className="flex flex-col md:flex-row lg:flex-row items-start justify-between text-2xl">
           <p>Gold Sponsor</p>
-          <p>₹ 50,000</p>
+          <p>₹ 3,50,000</p>
         </div>
         <div className="text-lg py-10">
-          <p>&rarr; All benefits of Silver Sponsor.</p>
+          <p>&rarr; Company will be identified as a co-sponsor for ic-ETITE 2024.</p>
           <p>
-            &rarr; Two additional corporate registrations for the conference.
+            &rarr; Branding of the Company around the campus during the fest through LED screens (more than 40-30 times a day).
           </p>
-          <p>&rarr; One booth in the conference registration area.</p>
+          <p>&rarr; Publicity on campus at the Academic Blocks (during Pre-ic-ETITE and all two days of ic-ETITE).</p>
           <p>
             &rarr; One half page advertisement in specific conference program.
           </p>
           <p>
-            &rarr; Ability to sponsor additional conference items such as
-            workshops, receptions, programs, etc. with proper recognition in all
-            program materials and conference website.
+            &rarr; Nation-wide circulation of commercials through our online media partners.
           </p>
           <p>
-            &rarr; Logo presence on conference website with a hotlink to your
-            organization's website.
+            &rarr; Marketing through standees inside the campus everywhere.
+          </p>
+          <p>
+            &rarr; Advertisement on the ic-ETITE’24 website and mobile app
+          </p>
+          <p>
+            &rarr; Marketing through standees inside the campus everywhere.
+          </p>
+          <p>
+            &rarr; Name and Logo on the T-Shirt of the conference.
+          </p>
+          <p>
+            &rarr; Speaker session at the conference.
+          </p>
+          <p>
+            &rarr; Stalls 10x10 sq ft. for two days
           </p>
         </div>
 
         <div className="flex flex-col md:flex-row lg:flex-row items-start justify-between text-2xl">
           <p>Silver Sponsor</p>
-          <p>₹ 25,000</p>
+          <p>₹ 2,00,000</p>
         </div>
         <div className="text-lg py-10">
-          <p>&rarr; Recognition in the conference programs.</p>
-          <p>&rarr; One quarter page advertisement in conference program.</p>
+          <p>&rarr; Company will be identified as a sponsor for ic-ETITE 2024. </p>
+          <p>&rarr; Branding of the Company around the campus during the fest through LED screens (more than 25-10 times a day). </p>
           <p>
-            &rarr; Permanent recognition in specific conference proceedings.
+            &rarr; Publicity on campus at the Academic Blocks (during Pre-ic-ETITE and all two days of ic-ETITE).
           </p>
           <p>
-            &rarr; Recognition of the firm's representatives at the conference,
-            with the opportunity to meet and interact with internationally
-            recognized figures in the Information Technology and Engineering
-            field.
+            &rarr; Nation-wide circulation of commercials through our online media partners.
           </p>
           <p>
-            &rarr; A private reception at the conference exclusively for
-            sponsors and members of the Conference committee.
+            &rarr; Marketing through standees inside the campus everywhere.
+          </p>
+          <p>
+            &rarr; Advertisement on the ic-ETITE’24 website and mobile app.
+          </p>
+          <p>
+            &rarr; Name and Logo on the T-Shirt of the conference.
+          </p>
+          <p>
+            &rarr; Speaker session at the conference
+          </p>
+          <p>
+            &rarr; Stalls 5x5 sq ft. for one day.
           </p>
         </div>
       </div>
