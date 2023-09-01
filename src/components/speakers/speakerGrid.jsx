2b13eb4b13effbb377a41d12aa0898ade20a3780
souvik150 <souvikmukherjee150@gmail.com>
2023-09-02 02:52:04 +0530

    feat: adds speakers

--- src/components/speakers/speakerGrid.jsx
+++ src/components/speakers/speakerGrid.jsx
@@ -10,6 +10,18 @@ import Korhan from "./../../../public/Korhan.png";
 import Rahul from "./../../../public/Rahul.png";
 import Tokunbo from "./../../../public/Tokunbo.png";
 import Vladimir from "./../../../public/vladimir-brusic.png";
+import Dave from "./../../../public/images/speakers/Dave Cliff.png";
+import Ayan from "./../../../public/images/speakers/Ayan.png";
+import Ashutosh from "./../../../public/images/speakers/Ashutosh.png";
+import Dileep from "./../../../public/images/speakers/Dr.png";
+import Peddoju from "./../../../public/images/speakers/Dr.Peddoju.png";
+import Jimson from "./../../../public/images/speakers/Jimson.png";
+import Narendra from "./../../../public/images/speakers/Narendra.png";
+import Rajat from "./../../../public/images/speakers/Rajat.png";
+import Saravana from "./../../../public/images/speakers/Saravana.png";
+import Shivashankar from "./../../../public/images/speakers/Shivashankar.png";
+import Shrikanth from "./../../../public/images/speakers/Shrikanth.png";
+
 
 const data = [
   {
@@ -55,6 +67,13 @@ const data = [
     college: "University of Fujairah, UAE",
   },
   {
+    id: 10,
+    image: Dave,
+    name: "Dr. Dave Cliff",
+    department: "School of Engineering Mathematics Technology",
+    college: "University of Bristol, UK",
+  },
+  {
     id: 7,
     image: SpeakerImg,
     name: "Dr. Srinivas Talabattula",
@@ -69,13 +88,73 @@ const data = [
     college: "Microsoft",
   },
   {
-    id: 9,
-    image: Rahul,
-    name: "Dr. Rahul Sharad Gaikwad",
-    department: "",
-    college: "Amazon",
+    id: 11,
+    image: Narendra,
+    name: "Dr. Narendra S Chaudhari",
+    department: "Vice Chancellor",
+    college: "Assam Science and Technology University, Gauhati",
   },
-
+  {
+    id: 12,
+    image: Shivashankar,
+    name: "Dr. Shivashankar B. Nair Professor",
+    department: "Department of Computer Science & Engineering",
+    college: "Indian Institute of Technology, Guwahati",
+  },
+  {
+    id: 13,
+    image: Rajat,
+    name: "Dr. Rajat Subhra Chakraborty",
+    department: "Department of Computer Science & Engineering",
+    college: "Indian Institute of Technology, Kharagpur",
+  },  {
+    id: 14,
+    image: Jimson,
+    name: "Dr. Jimson Mathew",
+    department: "Department of Computer Science & Engineering",
+    college: "Indian Institute of Technology, Patna",
+  },  {
+    id: 15,
+    image: Dileep,
+    name: "Dr. Dileep A. D",
+    department: "School of Computing and Electrical Engineering",
+    college: "Indian Institute of Technology, Mandi",
+  },
+  {
+    id: 16,
+    image: Peddoju,
+    name: "Dr.Peddoju Sateesh Kumar",
+    department: "Department of Computer Science and Engineering",
+    college: "Indian Institute of Technology, Roorkee",
+  },
+  {
+    id: 17,
+    image: Ashutosh,
+    name: "Dr. Ashutosh Modi",
+    department: "Department of Computer Science and Engineering",
+    college: "Indian Institute of Technology, Kanpur",
+  },
+  {
+    id: 18,
+    image: Ayan,
+    name: "Dr. Ayan Mondal",
+    department: "Department of Computer Science and Engineering",
+    college: "Indian Institute of Technology, Indore",
+  },
+  {
+    id: 19,
+    image: Saravana,
+    name: "Dr. Saravana Kumar M",
+    department: "Department of Computer Science and Engineering",
+    college: "Indian Institute of Technology, Roorkee",
+  },
+  {
+    id: 20,
+    image: Shrikanth,
+    name: "Dr. G. Shrikanth Reddy",
+    department: "School of Computing and Electrical Engineering",
+    college: "Indian Institute of Technology, Mandi",
+  },
 ];
 
 const SpeakerGrid = () => {
