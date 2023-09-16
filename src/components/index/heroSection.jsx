5565612be598c8da0f7848955b4934876c49f292
souvik150 <souvikmukherjee150@gmail.com>
2023-09-16 16:25:15 +0530

    feat: adds

--- src/components/index/heroSection.jsx
+++ src/components/index/heroSection.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import Image from "next/image";
 import BgImage from "./../../../public/images/vit.png";
@@ -8,28 +8,28 @@ import BgImageMobile from "../../../public/images/vit-mobile.png";
 import BgImageDesktop from "../../../public/images/vit.png";
 
 const heroSection = () => {
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-        // Add event listener for window resize
-        window.addEventListener("resize", handleResize);
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
 
-        // Initial check for mobile on component mount
-        handleResize();
+    // Initial check for mobile on component mount
+    handleResize();
 
-        // Clean up the event listener on component unmount
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    const bgImage = isMobile ? BgImageMobile : BgImageDesktop;
+  const bgImage = isMobile ? BgImageMobile : BgImageDesktop;
 
-    return (
+  return (
     <div className="relative h-[92vh] bg-cover z-0">
       <Image
         src={bgImage}
@@ -40,24 +40,30 @@ const heroSection = () => {
       />
       <div className="absolute inset-0 bg-gradient-to-r from-[#111042] z-20" />
       <div className="absolute top-0 left-0 z-30 text-white mx-8 md:mx-16 lg:mx-16 flex flex-col h-full justify-center w-[80vw] md:w-[70vw] lg:w-[50vw]">
-          <h1 className="text-2xl md:text-4xl lg:text-4xl font-bold">
-          Welcome to ic-ETITE'24!
+        <h1 className="text-2xl md:text-4xl lg:text-4xl font-bold">
+          to ic-ETITE'24!
         </h1>
 
         <p className="text-xl md:text-2xl lg:text-2xl my-6">
           Second International Conference on Emerging Trends in Information
           Technology and Engineering(ic-ETITE'24)
         </p>
-          <p className="text-xl md:text-2xl lg:text-2xl text-white">
+        <p className="text-xl md:text-2xl lg:text-2xl text-white">
           Technically co-sponsored by
-      </p>
-          <p className="text-2xl md:text-2xl lg:text-2xl mb-6 text-white"><span className="font-bold"> IEEE </span>Madras Section</p>
+        </p>
+        <p className="text-2xl md:text-2xl lg:text-2xl mb-6 text-white">
+          <span className="font-bold"> IEEE </span>Madras Section
+        </p>
         <p className="text-xl md:text-2xl lg:text-2xl mt-6 mb-16">
           February 22-23, 2024 at VIT Vellore, India
         </p>
-          <a href="https://drive.google.com/file/d/1E-joFjEGhLs4ctEda9PocxIwZftvdvRW/view?usp=sharing" target="_blank" className="w-fit border-2 border-white bg-transparent bg-white hover:bg-transparent hover:text-white text-[#111042] py-2 px-4 text-base sm:text-lg font-semibold rounded-md shadow z-40 cursor-pointer transition-all">
-              Download Brochure
-          </a>
+        <a
+          href="https://drive.google.com/file/d/1E-joFjEGhLs4ctEda9PocxIwZftvdvRW/view?usp=sharing"
+          target="_blank"
+          className="w-fit border-2 border-white bg-transparent bg-white hover:bg-transparent hover:text-white text-[#111042] py-2 px-4 text-base sm:text-lg font-semibold rounded-md shadow z-40 cursor-pointer transition-all"
+        >
+          Download Brochure
+        </a>
       </div>
     </div>
   );
